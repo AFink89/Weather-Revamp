15e57196087629024b0e3901367377a550861ea8
AFink89 <95246207+AFink89@users.noreply.github.com>
2022-02-03 10:04:15 -0700

    Created function display, elements...

--- src/script.js
+++ src/script.js
@@ -1,5 +1,25 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector(`#temperature`);
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector(`#city`);
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector(`#description`);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let sunriseElement = document.querySelector(`#sunrise`);
+  sunriseElement.innerHTML = response.data.sys.sunrise;
+  let sunsetElement = document.querySelector(`#sunset`);
+  sunsetElement.innerHTML = response.data.sys.sunset;
+  let humidityElement = document.querySelector(`#humidity`);
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector(`#wind`);
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
 let apiKey = "06dbfbcd1325d3e522edc830767aa8d4";
-let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=Phoenix&appid=${apiKey}";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Phoenix&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
 
 console.log(apiUrl);
